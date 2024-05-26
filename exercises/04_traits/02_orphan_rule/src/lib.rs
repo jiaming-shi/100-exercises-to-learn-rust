f0464ee0aa77958f5bbecbf3c5f63861af1dc551
Jiaming Shi <jiaming.shih@gmail.com>
2024-05-26 11:41:40 +0800

    add solutions for chapter 4

--- exercises/04_traits/02_orphan_rule/src/lib.rs
+++ exercises/04_traits/02_orphan_rule/src/lib.rs
@@ -4,8 +4,17 @@
 //  Look at the compiler error to get familiar with what it looks like.
 //  Then delete the code below and move on to the next exercise.
 
-impl PartialEq for u32 {
-    fn eq(&self, _other: &Self) -> bool {
-        todo!()
-    }
-}
+// error[E0117]: only traits defined in the current crate can be implemented for primitive types
+//  --> exercises/04_traits/02_orphan_rule/src/lib.rs:7:1
+//   |
+// 7 | impl PartialEq for u32 {
+//   | ^^^^^---------^^^^^---
+//   | |    |             |
+//   | |    |             `u32` is not defined in the current crate
+//   | |    `u32` is not defined in the current crate
+//   | impl doesn't use only types from inside the current crate
+//   |
+//   = note: define and implement a trait or new type instead
+
+// For more information about this error, try `rustc --explain E0117`.
+// error: could not compile `orphan` (lib) due to 1 previous error
--- exercises/04_traits/08_sized/src/lib.rs
+++ exercises/04_traits/08_sized/src/lib.rs
@@ -3,5 +3,17 @@ pub fn example() {
     // via `std::mem::size_of` will result in a compile-time error.
     //
     // TODO: Comment out the following line and move on to the next exercise.
-    std::mem::size_of::<str>();
+    // std::mem::size_of::<str>();
+    // error[E0277]: the size for values of type `str` cannot be known at compilation time
+    // --> exercises/04_traits/08_sized/src/lib.rs:6:25
+    // |
+    // 6 |     std::mem::size_of::<str>();
+    // |                         ^^^ doesn't have a size known at compile-time
+    // |
+    // = help: the trait `Sized` is not implemented for `str`
+    // note: required by an implicit `Sized` bound in `std::mem::size_of`
+    // --> /private/tmp/rust-20240518-7986-rmyaj5/rustc-1.78.0-src/library/core/src/mem/mod.rs:312:1
+
+    // For more information about this error, try `rustc --explain E0277`.
+    // error: could not compile `sized` (lib) due to 1 previous error
 }
