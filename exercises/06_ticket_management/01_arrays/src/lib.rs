3c7b032e8181a7188e23a88452d642813ce227d1
Jiaming Shi <jiaming.shih@gmail.com>
2024-06-02 22:37:52 +0800

    add solutions for chapter 6

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -2,6 +2,7 @@
 
 pub struct WeekTemperatures {
     // TODO
+    temperature: [Option<i32>; 7],
 }
 
 pub enum Weekday {
@@ -14,17 +15,29 @@ pub enum Weekday {
     Sunday,
 }
 
+fn weekday2index(day: &Weekday) -> usize {
+    match day {
+        Weekday::Monday => 0,
+        Weekday::Tuesday => 1,
+        Weekday::Wednesday => 2,
+        Weekday::Thursday => 3,
+        Weekday::Friday => 4,
+        Weekday::Saturday => 5,
+        Weekday::Sunday => 6,
+    }
+}
+
 impl WeekTemperatures {
     pub fn new() -> Self {
-        todo!()
+        WeekTemperatures { temperature: [None; 7] }
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        todo!()
+        self.temperature[weekday2index(&day)]
     }
 
     pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        todo!()
+        self.temperature[weekday2index(&day)] = Some(temperature);
     }
 }
 
